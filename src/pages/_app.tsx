61940108f09dd51d5901a71e26078e60c99d0e80
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-11-03 18:20:25 +0530

    dev: base setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import "@/style/globals.css";
+
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (    
+      <Component {...pageProps} />
+  );
+};
+
+export default MyApp;
